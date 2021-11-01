a2e379d9f526b14d93682b8388fc77bfc954f8f7
IlanReinstein <ilanreinstein@gmail.com>
2021-11-01 17:21:05 -0400

    updating bucket terraform config

--- gcp/modules/gcs_buckets/main.tf
+++ gcp/modules/gcs_buckets/main.tf
@@ -1,7 +1,7 @@
 locals {
   bucket_name = "${var.bucket_name}-${local.project_id}"
-  location    = var.region != "" ? var.region : data.google_client_config.current.region
-  project_id  = var.project_id != "" ? var.project_id : data.google_client_config.current.project
+  location    = var.region
+  project_id  = var.project_id
 }
 
 resource "google_storage_bucket" "capstone-bucket" {
