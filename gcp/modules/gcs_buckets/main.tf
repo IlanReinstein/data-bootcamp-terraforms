5e0afbb8eb791ccac3ff22f7bf7f28000f959a58
IlanReinstein <ilanreinstein@gmail.com>
2021-11-02 16:57:07 -0400

    removing iam resources

--- gcp/modules/gcs_buckets/main.tf
+++ gcp/modules/gcs_buckets/main.tf
@@ -9,31 +9,3 @@ resource "google_storage_bucket" "capstone-bucket" {
   location      = local.location
   project       = local.project_id
 }
-
-// Service Account
-resource "google_service_account" "storage_reader_writer" {
-  account_id = "storagereadwrite"
-  display_name = "Bucket reader writer"
-}
-
-// Service Account Key
-resource "google_service_account_key" "sa_key" {
-  service_account_id = google_service_account.storage_reader_writer.name
-}
-
-// IAM Bindings
-resource "google_storage_bucket_iam_binding" "bucket_creator" {
-  bucket = "${google_storage_bucket.capstone-bucket.name}"
-  role = "roles/storage.objectCreator"
-  members = [
-    join(":", ["serviceAccount", google_service_account.storage_reader_writer.email])
-  ]
-}
-
-resource "google_storage_bucket_iam_binding" "bucket_reader" {
-  bucket = "${google_storage_bucket.capstone-bucket.name}"
-  role = "roles/storage.objectViewer"
-  members = [
-    join(":", ["serviceAccount", google_service_account.storage_reader_writer.email])
-  ]
-}
