4fabac2ab8d0dafd472e4222d71afbe8020eaf90
IlanReinstein <ilanreinstein@gmail.com>
2021-11-11 18:06:01 -0500

    cluster creation corrected

--- gcp/modules/gcs_buckets/main.tf
+++ gcp/modules/gcs_buckets/main.tf
@@ -17,7 +17,6 @@ resource "google_storage_bucket" "rugged_buckets" {
 // Service Account
 resource "google_service_account" "airflow" {
   account_id = "airflow"
-  project_id = locals.project_id
   display_name = "Bucket reader writer"
 }
 
