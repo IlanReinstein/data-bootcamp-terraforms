f637523a37e53e4f41b486c5abe87f1cbe4b66d0
IlanReinstein <ilanreinstein@gmail.com>
2021-11-11 18:36:09 -0500

    cluster creation corrected

--- gcp/modules/dataproc/variables.tf
+++ gcp/modules/dataproc/variables.tf
@@ -1,6 +1,5 @@
 variable "dataproc_name" {
   description = "The name of the Google Cloud Dataproc Cluster."
-  type = "string"
 }
 variable "project_id" {
   description = "The ID of the google project to which the resource belongs. If it is not provided, the provider project is used."
