14fd071ef9348b65513fdea2faa1d7308c6da12b
IlanReinstein <ilanreinstein@gmail.com>
2021-11-11 17:53:27 -0500

    adding terraform cluster

--- gcp/modules/gcs_buckets/variables.tf
+++ gcp/modules/gcs_buckets/variables.tf
@@ -1,6 +1,3 @@
-variable "bucket_name" {
-  description = "The name of the Google Cloud Storage bucket. Must be globally unique."
-}
 
 variable "project_id" {
   description = "The ID of the google project to which the resource belongs. If it is not provided, the provider project is used."
