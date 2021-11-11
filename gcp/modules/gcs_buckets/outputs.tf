0b8862a958b966a2bb5d8f4f43a1307f04079f6d
IlanReinstein <ilanreinstein@gmail.com>
2021-11-11 17:50:45 -0500

    adding terraform cluster

--- gcp/modules/gcs_buckets/outputs.tf
+++ gcp/modules/gcs_buckets/outputs.tf
@@ -1,7 +1,1 @@
-output "bucket_id" {
-  value = google_storage_bucket.capstone-bucket.id
-}
-output "name" {
-  value       = google_storage_bucket.capstone-bucket.name
-  description = "The name of bucket."
-}
+
