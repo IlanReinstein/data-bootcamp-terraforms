8a6ced1c7c1ca48570acfbe7a39b5b4b082edd5a
IlanReinstein <ilanreinstein@gmail.com>
2021-11-11 18:17:19 -0500

    cluster creation corrected

--- gcp/modules/dataproc/main.tf
+++ gcp/modules/dataproc/main.tf
@@ -1,5 +1,64 @@
 // Dataproc Cluster
+resource "google_service_account" "default" {
+  account_id   = "capstone-default"
+  display_name = "Service Account"
+}
+
 resource "google_dataproc_cluster" "capstone-cluster" {
-  name   = var.cluster_name
-  region = var.region
+  name     = var.cluster_name
+  region   = "us-central1"
+  graceful_decommission_timeout = "120s"
+  labels = {
+    foo = "bar"
+  }
+
+  cluster_config {
+    staging_bucket = "stage"
+
+    master_config {
+      num_instances = 1
+      machine_type  = "e2-medium"
+      disk_config {
+        boot_disk_type    = "pd-ssd"
+        boot_disk_size_gb = 30
+      }
+    }
+
+    worker_config {
+      num_instances    = 2
+      machine_type     = "e2-medium"
+      min_cpu_platform = "Intel Skylake"
+      disk_config {
+        boot_disk_size_gb = 30
+        num_local_ssds    = 1
+      }
+    }
+
+    preemptible_worker_config {
+      num_instances = 0
+    }
+
+    # Override or set some custom properties
+    software_config {
+      image_version = "1.3.7-deb9"
+      override_properties = {
+        "dataproc:dataproc.allow.zero.workers" = "true"
+      }
+    }
+
+    gce_cluster_config {
+      tags = ["foo", "bar"]
+      # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+      service_account = google_service_account.default.email
+      service_account_scopes = [
+        "cloud-platform"
+      ]
+    }
+
+    # You can define multiple initialization_action blocks
+    initialization_action {
+      script      = "gs://dataproc-initialization-actions/stackdriver/stackdriver.sh"
+      timeout_sec = 500
+    }
+  }
 }
